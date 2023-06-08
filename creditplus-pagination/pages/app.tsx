b8b57e97b3ecbae8e6e56a95a374dc62b5502dbe
unknown <m.jeyavathanan@kopfundbyte.de>
2023-06-09 00:29:16 +0200

    dunno

--- creditplus-pagination/pages/app.tsx
+++ creditplus-pagination/pages/app.tsx
@@ -1,17 +1,17 @@
-// import { AppProps } from 'next/app';
-// import { GoogleFonts } from 'next-google-fonts';
-// import { fas } from '@fortawesome/free-solid-svg-icons';
-// import { library } from '@fortawesome/fontawesome-svg-core';
+import { AppProps } from 'next/app';
+import { GoogleFonts } from 'next-google-fonts';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
-// library.add(fas);
+library.add(fas);
 
-// function MyApp({ Component, pageProps }: AppProps) {
-//   return (
-//     <>
-//       <GoogleFonts href="https://fonts.googleapis.com/css2?family=Gotham:wght@400;500;600&display=swap" />
-//       <Component {...pageProps} />
-//     </>
-//   );
-// }
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Gotham:wght@400;500;600&display=swap" />
+      <Component {...pageProps} />
+    </>
+  );
+}
 
-// export default MyApp;
+export default MyApp;

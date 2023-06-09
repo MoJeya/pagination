f7d9258a2bf3ac305987d6064061c43a35a036e9
unknown <m.jeyavathanan@kopfundbyte.de>
2023-06-09 14:19:58 +0200

    done to my best of abbilyties

--- creditplus-pagination/src/app/components/Header.tsx
+++ creditplus-pagination/src/app/components/Header.tsx
@@ -1,3 +1,71 @@
+// import React, { useState } from "react";
+// import Navbar from "./base-components/Navbar";
+// import styled from "styled-components";
+// import DropdownPicker from "./base-components/DropdownPicker";
+// import NumberedNavigation from "./base-components/NumberdNavigation";
+
+// const StyledHeader = styled.header`
+//     background-color: #FAFAFA;
+//     color: white;
+//     top: 81px;
+//     left: 0px;
+//     width: 100%;
+//     height: 398px;
+// `;
+
+// const HeaderContainer = styled.div`
+//   width: 100%;
+//   height: 318px;
+//   display: flex;
+//   justify-content: center;
+// `;
+
+// const MainHeaderContainer = styled.div`
+//   width: 965px;
+//   height: 158px;
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+// const H3 = styled.span`
+//   margin-top: 80px;
+//   color: #1AAEB7;
+//   text-align: center;
+//   font-family: 'Gotham', sans-serif;
+//   font-weight: bold;
+//   font-size: 16px;
+//   letter-spacing: -0.32px;
+// `;
+
+// const H1Container = styled.div`
+//   width: 824px;
+//   height: 48px;
+//   display: contents;
+
+// `;
+
+// const H1 = styled.span`
+//   color: #004B44;
+//   font-family: 'Gotham', sans-serif;
+//   font-weight: bold;
+//   font-size: 48px;
+//   letter-spacing: -1.44px;
+//   text-align: center;
+// `;
+
+// const DropDownContainer = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   justify-content: space-between;
+//   @media (max-width: 768px) {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     width: auto;
+//     max-width: 674px;
+//   }
+// `;
+
 import React, { useState } from "react";
 import Navbar from "./base-components/Navbar";
 import styled from "styled-components";
@@ -5,58 +73,63 @@ import DropdownPicker from "./base-components/DropdownPicker";
 import NumberedNavigation from "./base-components/NumberdNavigation";
 
 const StyledHeader = styled.header`
-    background-color: #FAFAFA;
-    color: white;
-    top: 81px;
-    left: 0px;
-    width: 100%;
-    height: 398px;
+  background-color: #fafafa;
+  color: white;
+  top: 81px;
+  left: 0px;
+  width: 100%;
+  height: auto;
+  padding: 1rem;
 `;
 
 const HeaderContainer = styled.div`
   width: 100%;
-  height: 318px;
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
 `;
 
 const MainHeaderContainer = styled.div`
-  width: 965px;
-  height: 158px;
+  width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 const H3 = styled.span`
-  margin-top: 80px;
-  color: #1AAEB7;
+  margin-top: 1rem;
+  color: #1aaeb7;
   text-align: center;
-  font-family: 'Gotham', sans-serif;
+  font-family: "Gotham", sans-serif;
   font-weight: bold;
   font-size: 16px;
   letter-spacing: -0.32px;
 `;
 
 const H1Container = styled.div`
-  width: 824px;
-  height: 48px;
-  display: contents;
-
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
 `;
 
 const H1 = styled.span`
-  color: #004B44;
-  font-family: 'Gotham', sans-serif;
+  color: #004b44;
+  font-family: "Gotham", sans-serif;
   font-weight: bold;
   font-size: 48px;
-  letter-spacing: -1.44px;
+  letter-spacing: -0.72px;
   text-align: center;
 `;
 
 const DropDownContainer = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const DropdownPickerWrapper = styled.div`
+  margin: 0.5rem;
 `;
 
 interface IJobs {
@@ -74,22 +147,28 @@ const Header: React.FC <IJobs>= (props: IJobs) => {
   
 
   return(
-    <StyledHeader>
-      <Navbar />
-      <HeaderContainer>
-        <MainHeaderContainer>
-          <H3>{props.amountOfJobs} offene Stellen bei Creditplus</H3>
-          <H1Container>
-            <H1>Hier beginnt deine Zukunft</H1>
-          </H1Container>
-          <DropDownContainer>
-            <DropdownPicker title={"Wähle einen Standort"} isOpen={dropdownStates[0]} toggleDropdown={() => toggleDropdown(0)}/>
-            <DropdownPicker title={"Wähle eine Stadt"} isOpen={dropdownStates[1]} toggleDropdown={() => toggleDropdown(1)}/>
-            <DropdownPicker title={"Wähle dein Erfahrungslevel"} isOpen={dropdownStates[2]} toggleDropdown={() => toggleDropdown(2)}/>
-          </DropDownContainer>
-        </MainHeaderContainer>
-      </HeaderContainer>
-    </StyledHeader>
+      <StyledHeader>
+        <Navbar />
+        <HeaderContainer>
+          <MainHeaderContainer>
+            <H3>{props.amountOfJobs} offene Stellen bei Creditplus</H3>
+            <H1Container>
+              <H1>Hier beginnt deine Zukunft</H1>
+            </H1Container>
+            <DropDownContainer>
+              <DropdownPickerWrapper>
+                <DropdownPicker title={"Wähle einen Standort"} isOpen={dropdownStates[0]} toggleDropdown={() => toggleDropdown(0)} />
+              </DropdownPickerWrapper>
+              <DropdownPickerWrapper>
+                <DropdownPicker title={"Wähle eine Stadt"} isOpen={dropdownStates[1]} toggleDropdown={() => toggleDropdown(1)} />
+              </DropdownPickerWrapper>
+              <DropdownPickerWrapper>
+                <DropdownPicker title={"Wähle dein Erfahrungslevel"} isOpen={dropdownStates[2]} toggleDropdown={() => toggleDropdown(2)} />
+              </DropdownPickerWrapper>
+            </DropDownContainer>
+          </MainHeaderContainer>
+        </HeaderContainer>
+      </StyledHeader>
     );
   };
   

2ff5847d1641d0ebe4646ea45a028dbdc32b507e
unknown <m.jeyavathanan@kopfundbyte.de>
2023-06-26 15:03:02 +0200

    fix 1

--- creditplus-pagination/next.config.js
+++ creditplus-pagination/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // css: ['./styles/global.css'],
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true
+    }
 }
 
 module.exports = nextConfig
--- creditplus-pagination/pages/_app.tsx
+++ creditplus-pagination/pages/_app.tsx
@@ -1,25 +1,17 @@
 import { AppProps } from 'next/app';
-import { GoogleFonts } from 'next-google-fonts';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import GlobalStyles from '../styles/globalStyles';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
-
-
-
-
 function MyApp({ Component, pageProps }: AppProps) {
-
   const queryClient = new QueryClient();
-  
 
   return (
     <QueryClientProvider client={queryClient}>
-      <GlobalStyles/>
-      {/* <GoogleFonts href="https://fonts.googleapis.com/css2?family=Gotham:wght@400;500;600&display=swap" /> */}
+      <GlobalStyles />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
 }
+
 export default MyApp;
